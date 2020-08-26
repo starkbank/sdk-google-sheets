9a6d66b5b1ce7445ec915912d6aed4e624bf7818
Lucas <lucas.tnagel@gmail.com>
2020-08-26 09:39:12 -0300

    Read privatekey implemented

--- sdkSendTransfer.js
+++ sdkSendTransfer.js
@@ -1,5 +1,4 @@
-function sendTransferDialog()
-{
+function sendTransferDialog() {
     var html = HtmlService.createHtmlOutputFromFile('FormSendOrder');
     SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
     .showModalDialog(html, 'Envio de Transferência sem Aprovação');
