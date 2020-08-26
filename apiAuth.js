cf05a43b57da86146ba0a283ab058078d09a9536
Lucas <lucas.tnagel@gmail.com>
2020-08-26 08:36:01 -0300

    Fixing wrong password bug

--- apiAuth.js
+++ apiAuth.js
@@ -33,6 +33,18 @@ function getUserInputCredential(email, workspace, password, environment) {
   SaveCredentials(workspace, email, environment, accessToken, memberName, workspaceId);
 }
 
+function verifyPassword(password) {
+  let user = new getDefaultUser();
+
+  payload = {
+    workspace: user.workspace,
+    email: user.email,
+    password: password,
+    platform: "api"
+  }
+  fetch("/auth/access-token", method = 'POST', payload, null, "v1", user.environment);
+}
+
 function SaveCredentials(workspace, email, environment, accessToken, membername, workspaceId) {
   let sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Credentials');
   sheet.getRange(1,2,8,1).clearContent();
