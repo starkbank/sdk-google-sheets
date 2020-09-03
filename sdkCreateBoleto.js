f942d4642712b2b55d19c83766618a416c6d6d5b
Lucas <lucas.tnagel@gmail.com>
2020-09-03 16:24:02 -0300

    Continue implementation of createBoleto

--- sdkCreateBoleto.js
+++ sdkCreateBoleto.js
@@ -7,16 +7,78 @@ function createBoleto()
     formatHeader(sheet);
 
     for(let i=11; i<=sheet.getLastRow(); i++) {
+
+        let descriptions = [];
+        for(let j=0; j<3; j++)
+        {
+            let column = String.fromCharCode(73 + j);
+            let text = sheet.getRange(column + i.toString()).getValue();
+            if(text.lenght > 0)
+            {
+                let description = {
+                    text: text,
+                    amount: parseInt(sheet.getRange(column + i.toString()).getValue())
+                };
+                descriptions.push(description);
+            }
+        }
+
         let boleto = {
             customerId: sheet.getRange('A' + i.toString()).getValue(),
-            amount: parseInt(100*sheet.getRange('B' + i.toString()).getValue(), 10),
-            dueDate: formatDateToISO(sheet.getRange('C' + i.toString()).getValue()),
-            fine: parseFloat(sheet.getRange('D' + i.toString().getValue())),
-            interest: parseFloat(sheet.getRange('E' + i.toString()).getValue()),
-            overdueLimit: parseInt(sheet.getRange('F' + i.toString()).getValue()),
-            discount: parseFloat(sheet.getRange('G' + i.toString()).getValue()),
-            discountDate: formatDateToISO(sheet.getRange('H' + i.toString()).getValue()),
-            
+            amount: parseInt(100*sheet.getRange('B' + i.toString()).getValue(), 10)
+        };
+
+        let dueDate = sheet.getRange('C' + i.toString()).getValue();
+        if(dueDate.length > 0)
+        {
+            boleto["dueDate"] = formatDateToISO(dueDate);
         }
+        let fine = sheet.getRange('D' + i.toString()).getValue();
+        if(fine.length > 0)
+        {
+            boleto["fine"] = parseFloat(fine);
+        }
+        let interest = sheet.getRange('E' + i.toString()).getValue();
+        if(interest.length > 0)
+        {
+            boleto["interest"] = parseFloat(interest);
+        }
+        let overdueLimit = sheet.getRange('F' + i.toString()).getValue();
+        if(overdueLimit.length > 0)
+        {
+            boleto["overdueLimit"] = parseInt(overdueLimit);
+        }
+        let discount = sheet.getRange('G' + i.toString()).getValue();
+        if(discount.length > 0)
+        {
+            boleto["dicount"] = parseFloat(discount);
+        }
+        let discountDate = sheet.getRange('H' + i.toString()).getValue();
+        if(discountDate.length > 0)
+        {
+            boleto["discountDate"] = formatDateToISO(discountDate);
+        }
+
+
+        if(descriptions.length > 0)
+        {
+            boleto["descriptions"] = descriptions;
+        }
+        let tags = sheet.getRange('O' + i.toString()).getValue().split(",");
+        if(tags.lenght > 0)
+        {
+            boleto["tags"] = tags;
+        }
+
+        boletos.push(boleto);
     }
+
+    let payload = {
+        charges: boletos
+    };
+    
+    Browser.msgBox(JSON.stringify(payload));
+    //throw new Error(payload);
+
+    //json = JSON.parse(fetch("/charge", method = 'POST', payload, null).content);
 }
