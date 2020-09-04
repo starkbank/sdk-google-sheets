469ae6e7e10044fa3e1b9d0acbf3bd8d9e796a66
Lucas <lucas.tnagel@gmail.com>
2020-09-04 06:32:49 -0300

    Implement CreateBoleto

--- sdkCreateBoleto.js
+++ sdkCreateBoleto.js
@@ -11,13 +11,14 @@ function createBoleto()
         let descriptions = [];
         for(let j=0; j<3; j++)
         {
-            let column = String.fromCharCode(73 + j);
-            let text = sheet.getRange(column + i.toString()).getValue();
-            if(text.lenght > 0)
+            let columnText = String.fromCharCode(73 + 2*j);
+            let text = sheet.getRange(columnText + i.toString()).getValue();
+            if(text.length > 0)
             {
+                let columnAmount = String.fromCharCode(73 + 2*j + 1);
                 let description = {
                     text: text,
-                    amount: parseInt(sheet.getRange(column + i.toString()).getValue())
+                    amount: parseInt(sheet.getRange(columnAmount + i.toString()).getValue())
                 };
                 descriptions.push(description);
             }
@@ -33,25 +34,25 @@ function createBoleto()
         {
             boleto["dueDate"] = formatDateToISO(dueDate);
         }
-        let fine = sheet.getRange('D' + i.toString()).getValue();
+        let fine = new String(sheet.getRange('D' + i.toString()).getValue());
         if(fine.length > 0)
         {
             boleto["fine"] = parseFloat(fine);
         }
-        let interest = sheet.getRange('E' + i.toString()).getValue();
+        let interest = new String(sheet.getRange('E' + i.toString()).getValue());
         if(interest.length > 0)
         {
             boleto["interest"] = parseFloat(interest);
         }
-        let overdueLimit = sheet.getRange('F' + i.toString()).getValue();
+        let overdueLimit = new String(sheet.getRange('F' + i.toString()).getValue());
         if(overdueLimit.length > 0)
         {
             boleto["overdueLimit"] = parseInt(overdueLimit);
         }
-        let discount = sheet.getRange('G' + i.toString()).getValue();
+        let discount = new String(sheet.getRange('G' + i.toString()).getValue());
         if(discount.length > 0)
         {
-            boleto["dicount"] = parseFloat(discount);
+            boleto["discount"] = parseFloat(discount);
         }
         let discountDate = sheet.getRange('H' + i.toString()).getValue();
         if(discountDate.length > 0)
@@ -65,7 +66,7 @@ function createBoleto()
             boleto["descriptions"] = descriptions;
         }
         let tags = sheet.getRange('O' + i.toString()).getValue().split(",");
-        if(tags.lenght > 0)
+        if(tags.length > 0)
         {
             boleto["tags"] = tags;
         }
@@ -76,9 +77,27 @@ function createBoleto()
     let payload = {
         charges: boletos
     };
-    
-    Browser.msgBox(JSON.stringify(payload));
-    //throw new Error(payload);
 
-    //json = JSON.parse(fetch("/charge", method = 'POST', payload, null).content);
+    try{
+        fetch("/charge", method = 'POST', payload, null);
+        Browser.msgBox("Boletos Emitidos");
+    } catch(e)
+    {
+        let message = "Boletos não emitidos. ";
+        let parsed = JSON.parse(e);
+        message += parsed["message"];
+        if("errors" in parsed)
+        {
+            for(let suberror of parsed["errors"])
+            {
+                let suberrorMessage = suberror["message"];
+                let splitMessage = suberrorMessage.split(":");
+                splitMessage[0] = splitMessage[0].replace("Element ", "");
+                splitMessage[0] = "Linha " + (10 - (-splitMessage[0]));
+                let newMessage = splitMessage.join(":");
+                message += "\n" + newMessage;
+            }
+        }
+        Browser.msgBox(message);
+    }
 }
